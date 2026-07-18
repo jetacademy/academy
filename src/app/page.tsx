a8d2cb11eab7d5eaccbd8a9a86445d413c2f7f14
jetacademy <najib.yusuf@yahoo.com>
2026-07-18 23:12:46 +0700

    remove: Testimonials section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import WaFloat from "@/components/WaFloat";
 import Faq from "@/components/Faq";
-import Testimonials from "@/components/Testimonials";
 import Icon, { TYPE_ICON } from "@/components/Icon";
 import { getPrograms } from "@/lib/programs";
 import { TYPE_LABEL, type ProgramType } from "@/lib/fallback";
@@ -246,8 +245,6 @@ export default async function Home() {
           </div>
         </div>
       </section>
-
-      <Testimonials limit={4} />
 
       <Footer />
       <WaFloat />
